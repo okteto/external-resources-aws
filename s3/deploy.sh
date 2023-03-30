70a9379a7f0d71a83848f28e0c14266dce934aa7
Ramiro Berrelleza <ramiro@okteto.com>
2023-03-30 13:09:45 -0700

    add docs to the manifest

--- s3/deploy.sh
+++ s3/deploy.sh
@@ -1,8 +1,10 @@
-bucketName=${OKTETO_NAMESPACE}-oktacoshop
-createOutput=$(aws s3 mb s3://$bucketName)
+#!/bin/sh
+
+bucketName="${OKTETO_NAMESPACE}-oktacoshop"
+aws s3 mb "s3://$bucketName"
 exitCode=$?
 
-if [ $exitCode -ne 0 && $exitCode -ne 254 ]; then
+if [ $exitCode -ne 0 ] && [ $exitCode -ne 254 ]; then
   echo "Failed to create S3: exit code $exitCode"
   exit $exitCode
 fi
@@ -18,9 +20,12 @@ fi
 
 dashboard="https://s3.console.aws.amazon.com/s3/buckets/$bucketName"
 
+{
+  echo "OKTETO_EXTERNAL_S3_ENDPOINTS_BUCKET_URL=$dashboard"
+  echo "S3_BUCKET_NAME=$bucketName"
+}  >> "$OKTETO_ENV"
 
-echo "OKTETO_EXTERNAL_S3_ENDPOINTS_BUCKET_URL=${dashboard}" >> $OKTETO_ENV
-echo "S3_BUCKET_NAME=$bucketName" >> $OKTETO_ENV
+ 
 echo "S3 bucket configuration generated successfully"
 exit 0
 
--- s3/destroy.sh
+++ s3/destroy.sh
@@ -1,7 +1,9 @@
-bucketName=${OKTETO_NAMESPACE}-oktacoshop
-output=$(aws s3api delete-bucket --bucket $bucketName)
+#!/bin/sh
+
+bucketName="${OKTETO_NAMESPACE}-oktacoshop"
+aws s3api delete-bucket --bucket "$bucketName"
 exitCode=$?
-if [ exitCode -eq 254 || exitCode -eq 0 ]; then
+if [ $exitCode -eq 254 ] || [ $exitCode -eq 0 ]; then
   echo "S3 bucket deleted successfully"
   exit 0
 fi
