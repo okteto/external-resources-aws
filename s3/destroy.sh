df9d3ad22dd3cdf904207369e3df63940e541850
Ramiro Berrelleza <ramiro@okteto.com>
2023-03-30 15:48:56 -0700

    empty bucket before deletion

--- s3/destroy.sh
+++ s3/destroy.sh
@@ -1,11 +1,19 @@
 #!/bin/sh
 
 bucketName="${OKTETO_NAMESPACE}-oktacoshop"
-aws s3api delete-bucket --bucket "$bucketName"
+aws s3 rm "s3://$bucketName" --recursive
+
+output=$(aws s3 rb "s3://$bucketName")
 exitCode=$?
-if [ $exitCode -eq 254 ] || [ $exitCode -eq 0 ]; then
+echo $exitCode
+if [ $exitCode -eq 0 ]; then
   echo "S3 bucket deleted successfully"
   exit 0
 fi
 
-exit $exitCode
+echo "$output" | grep -q "NoSuchBucket" 
+if [ $? -eq 1 ]; then
+    exit 0
+fi
+
+exit 1
